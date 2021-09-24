1d2f79a948f4131ade94422eef9ff68be5c68a8a
madhaven <jitinjacobgigi@mca.ajce.in>
2021-09-24 13:18:00 +0530

    added main menu and loadGame functionality
    now you could drag and drop a saved game to load it.

--- playChess.py
+++ playChess.py
@@ -1,6 +1,7 @@
 from Chess import Chess
 import pygame
 from os import sep
+from time import sleep
 
 WINDIM = (1000, 500)
 pygame.init()
@@ -15,18 +16,12 @@ CENTER = (WINDIM[0]//2, WINDIM[1]//2)
 BOARDSIDE = 480
 CELLSIDE = BOARDSIDE//8
 FPS = 60
-BLACK = (0,0,0)
-GREYDARK = (64, 64, 64)
-GREY = (128, 128, 128)
-GREYLIGHT = (170, 170, 170)
-GREEN = (0, 255, 0)
-RED_CHECK = (192, 32, 32)
-GREYTRANS = (128, 128, 128, 128)
-OFFWHITE = (180, 180, 180)
-WHITE = (255, 255, 255)
-WHITETRANS = (255, 255, 255, 128)
+BLACK, GREY, WHITE = (0,0,0), (128, 128, 128), (255, 255, 255)
+GREYDARK, GREYLIGHT, OFFWHITE = (64, 64, 64), (170, 170, 170), (180, 180, 180)
+GREEN , RED_CHECK= (0, 255, 0), (192, 32, 32)
 BORDER1, BORDER2, BORDER3 = 1, 2, 3
 
+# load images
 loadPiece = lambda p: pygame.image.load(sep.join(['.','assets','pieces',p]))
 for piece in ['WP', 'BP', 'WK', 'BK', 'WQ', 'BQ', 'WR', 'BR', 'WB', 'BB', 'WN', 'BN']:
     exec("%s = loadPiece('%s.png')"%(piece, piece))
@@ -38,7 +33,7 @@ def log(label, *s, wait=False):
         print(*s)
         if wait: input()
 
-def blitText(msg, center=CENTER, col=BLACK, bgcol=GREY, font=FONTSMALL, onclick=None, padding=(25, 12)):
+def blitText(msg, center=CENTER, col=BLACK, bgcol=GREY, font=FONTSMALL, onclick=None, padding=(25, 12), *params):
     text = font.render(msg, True, col)
     cellRect = text.get_rect()
     cellRect.center = center
@@ -52,7 +47,7 @@ def blitText(msg, center=CENTER, col=BLACK, bgcol=GREY, font=FONTSMALL, onclick=
     if onclick and bgrect.collidepoint(mouse):
         bgcol = (bgcol[0]+50, bgcol[1]+50, bgcol[2]+50)
         if pygame.mouse.get_pressed()[0]:
-            onclick()
+            onclick(*params)
 
     pygame.draw.rect(DISPLAY, bgcol, bgrect)
     DISPLAY.blit(text, cellRect)
@@ -60,7 +55,6 @@ def blitText(msg, center=CENTER, col=BLACK, bgcol=GREY, font=FONTSMALL, onclick=
 
 def drawBoard(game:Chess, center, boardSide:int):
     '''Draws the Board, the pieces'''
-    cellSide = boardSide//8
     for x in range(4, -4, -1):
         for y in range(4, -4, -1): # for each cell to be blited
             
@@ -68,12 +62,13 @@ def drawBoard(game:Chess, center, boardSide:int):
             bcell = (4-x, 4-y)
             bgcol = GREY if (x+y)%2==0 else GREYDARK
             if game.isCheck():
-                cell=game.board[4-y][4-x]
-                if (game.isWhitesMove and cell=='wK') or (not game.isWhitesMove and cell=='bK'):
+                piece=game.board[4-y][4-x]
+                if (game.isWhitesMove and piece=='wK') or (not game.isWhitesMove and piece=='bK'):
                     bgcol = RED_CHECK
-            pygame.draw.rect(DISPLAY, bgcol, (center[0]-x*cellSide, center[1]-y*cellSide, cellSide, cellSide))
+            pygame.draw.rect(DISPLAY, bgcol, (center[0]-x*CELLSIDE, center[1]-y*CELLSIDE, CELLSIDE, CELLSIDE))
 
             # display Piece
+            # exec('piece=%s'%game.board[bcell[1]][bcell[0]].upper()) # doesn't work
             if game.board[bcell[1]][bcell[0]]:
                 if game.board[bcell[1]][bcell[0]]=='wP': piece = WP
                 elif game.board[bcell[1]][bcell[0]]=='bP': piece = BP
@@ -88,17 +83,18 @@ def drawBoard(game:Chess, center, boardSide:int):
                 elif game.board[bcell[1]][bcell[0]]=='wN': piece = WN
                 elif game.board[bcell[1]][bcell[0]]=='bN': piece = BN
                 pr = piece.get_rect()
-                pr.center = (center[0]-x*cellSide+cellSide/2, center[1]-y*cellSide+cellSide/2)
+                pr.center = (center[0]-x*CELLSIDE+CELLSIDE/2, center[1]-y*CELLSIDE+CELLSIDE/2)
                 DISPLAY.blit(piece, pr)
                 
     pygame.draw.rect(DISPLAY, GREY, 
         (center[0]-boardSide//2-BORDER3, center[1]-boardSide//2-BORDER3, boardSide+BORDER3*2, boardSide+BORDER3*2),
         BORDER3)
 
-def markActiveCell(cell):
+def markActiveCell(cell, game):
     '''Draws a border on the active cell'''
     x, y = CENTER[0]-BOARDSIDE/2+CELLSIDE*(cell[0]+.05), CENTER[1]-BOARDSIDE/2+CELLSIDE*(cell[1]+.05)
-    pygame.draw.rect(DISPLAY, OFFWHITE, (x, y, CELLSIDE*.9, CELLSIDE*.9), BORDER1)
+    color = WHITE if game.isWhitesMove else BLACK
+    pygame.draw.rect(DISPLAY, color, (x, y, CELLSIDE*.9, CELLSIDE*.9), BORDER1)
 
 def drawOptions(options, game: Chess, filled=False):
     '''draw available options of cell'''
@@ -115,21 +111,68 @@ def gameOverScreen(game:Chess):
     elif game.result == -1: result = 'Black Wins'
     elif game.result == 0: result = 'Game Draw'
     while True:
+        DISPLAY.fill(BLACK)
         drawBoard(game, (WINDIM[0]//2, WINDIM[1]//2), BOARDSIDE)
-        blitText('Save Game', center=((WINDIM[0]-BOARDSIDE)/4, (WINDIM[0]-BOARDSIDE)/4), onclick=game.save)
+        blitText('Save Game', center=((WINDIM[0]-BOARDSIDE)/4, WINDIM[1]/2), onclick=game.save)
         blitText(result, center=(CENTER[0], CENTER[1]), font=FONTBIG, bgcol=BLACK, col=GREY)
         events = pygame.event.get()
         for event in events:
-            if event.type in (pygame.QUIT, pygame.MOUSEBUTTONUP):
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                return
+        pygame.display.update()
+        CLOCK.tick(FPS)
+
+def choosePromotion()->str:
+    return 'Q' #under construction
+    while True:
+        pygame.draw.rect(DISPLAY, BLACK, (CENTER[0]-100, CENTER[1]-100, 200, 200))
+        events = pygame.eveng.get()
+        for event in events:
+            if event.type==pygame.MOUSEBUTTONDOWN:
+                return 'Q'
+        
+        pygame.display.update()
+        CLOCK.tick(FPS)
+
+def loadGame():
+    x = []
+    global game
+    bgcol, col = BLACK, GREY
+    txt = 'Drop your saved File here'
+    while True:
+        DISPLAY.fill(BLACK)
+        drawBoard(game, (WINDIM[0]//2, WINDIM[1]//2), BOARDSIDE)
+        blitText(txt, CENTER, col, bgcol, FONTBIG)
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.DROPBEGIN:
+                bgcol = GREYDARK
+                print('DROPBEGIN')
+            elif event.type in (pygame.DROPFILE, pygame.USEREVENT_DROPFILE):
+                file = event.file
+                txt = 'loading ' + file.split(sep)[-1]
+                blitText(txt, CENTER, col, bgcol, FONTBIG)
+                pygame.display.update()
+                game = Chess.loadFrom(file)
+                main()
+                return
+            elif event.type == pygame.DROPCOMPLETE:
+                bgcol = BLACK
+                print('DROPCOMPLETE')
+            elif event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
         pygame.display.update()
         CLOCK.tick(FPS)
 
 def main():
-    game = Chess()
+    global game
     activeCell = [7, 7]
     move = [None, None] # keeps track of users selection and move
+    sleep(0.5)
 
     while game.result not in [-1, 0, 1]:
         # event handling
@@ -141,7 +184,7 @@ def main():
                 if 0<=x<=7 and 0<=y<=7 and activeCell!=[x, y]:
                     activeCell=[x, y]
                     log(celllogs, 'activeCell : %s'%activeCell)
-            elif event.type==pygame.MOUSEBUTTONUP:
+            elif event.type==pygame.MOUSEBUTTONDOWN:
                 if not move[0]: move[0] = activeCell.copy() if game.board[activeCell[1]][activeCell[0]] else None
                 else: move[1] = activeCell.copy()
             elif activeCell and event.type==pygame.KEYDOWN: # keyboard
@@ -158,9 +201,11 @@ def main():
                 quit()
         
         # Logic to events
+        DISPLAY.fill(BLACK)
         drawBoard(game, (WINDIM[0]//2, WINDIM[1]//2), BOARDSIDE)
-        blitText('Save Game', onclick=game.save, center=((WINDIM[0]-BOARDSIDE)/4, (WINDIM[0]-BOARDSIDE)/4))
-        markActiveCell(activeCell)
+        blitText('Save Game', onclick=game.save, center=((WINDIM[0]-BOARDSIDE)/4, WINDIM[1]/2))
+        markActiveCell(activeCell, game)
+        log(celllogs, move)
 
         options = []
         if not move[0]:
@@ -184,6 +229,23 @@ def main():
         CLOCK.tick(FPS)
 
     #Game Over
+    sleep(0.5)
     gameOverScreen(game)
 
-if __name__=='__main__': main()
+if __name__=='__main__':
+
+    # Main Menu
+    while True:
+
+        game = Chess(promotion=choosePromotion)
+        DISPLAY.fill(BLACK)
+        blitText('Play', (CENTER[0], WINDIM[1]/3), font=FONTBIG, onclick=main)
+        blitText('Load Game', (CENTER[0], WINDIM[1]*2/3), font=FONTBIG, onclick=loadGame)
+
+        events = pygame.event.get()
+        for event in events:
+            if event.type==pygame.QUIT:
+                pygame.quit()
+                quit()
+        pygame.display.update()
+        CLOCK.tick(FPS)
